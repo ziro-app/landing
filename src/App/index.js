f1ae479be08f9cf5765049e93c17e57dc20d0291
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-11-08 21:25:11 -0800

    basic config for new project

---
+++ src/App/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Landing from '@bit/vitorbarbosa19.ziro.landing'
+import { container } from './styles'
+
+export const App = () =>
+	<div style={container}>
+		<Landing />
+	</div>
---
+++ src/App/styles.js
@@ -0,0 +1,9 @@
+export const
+
+container = {
+	maxWidth: '500px',
+	height: '100vh',
+	boxSizing: 'border-box',
+	margin: '0 auto',
+	padding: '20px 5% 0'
+}
