446e839e1a1eede4c0987ce3420b4448f54b5604
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-11-10 17:31:22 -0800

    remove parcel and use webpack to fix build error

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = (env, { mode }) => {
+	const config = {
+		module: {
+			rules: [
+				{
+					test: /\.js$/, 
+					exclude: /node_modules/, 
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env', '@babel/preset-react'],
+							plugins: ['@babel/plugin-transform-runtime']
+						}
+					}
+				},
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'raw-loader']
+				}
+			]
+		},
+		plugins: [ new HtmlWebpackPlugin({ template: './src/index.html' }) ]
+	}
+	if (mode === 'development') {
+		const { mapbox_api } = require('./credentials')
+		config.devtool = 'cheap-module-eval-source-map'
+		config.devServer = { historyApiFallback: true }
+		config.plugins.push(
+			new webpack.DefinePlugin({
+				'process.env': {
+					MAPBOX_API: JSON.stringify(mapbox_api)
+				}
+			})
+		)
+	}
+	if (mode === 'production') {
+		config.devtool = 'cheap-module-source-map'
+		config.plugins.push(
+			new CompressionPlugin(),
+			new webpack.DefinePlugin({
+				'process.env': {
+					MAPBOX_API: JSON.stringify(process.env.MAPBOX_API)
+				}
+			})
+		)
+	}
+	return config
+}
